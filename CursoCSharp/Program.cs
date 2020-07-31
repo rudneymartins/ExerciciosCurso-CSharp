91d60b04a586cd1f4c238f7fb0a46f0c2c5892fe
Rudney Martins <rudneymartins@gmail.com>
2020-07-31 17:58:11 -0300

    rud

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -4,6 +4,11 @@ namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+
+            {
+                Console.WriteLine("Hello World 2!");
+            }
         }
     }
 }
+ 
